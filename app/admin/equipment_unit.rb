bdc9336e81543105de50ee6df733af6221056820
Javier Fonseca V <fonsecajavier@gmail.com>
2017-10-03 19:47:17 -0500

    After creating a new equipment unit, redirect back to new equipment unit page

--- app/admin/equipment_unit.rb
+++ app/admin/equipment_unit.rb
@@ -2,6 +2,20 @@ ActiveAdmin.register EquipmentUnit do
   belongs_to :equipment_type
   permit_params :name, :initial_hourmeter
 
+  controller do
+    def after_save(success, failure, view_for_failure)
+      success.html { redirect_to new_admin_equipment_type_equipment_unit_path(resource.equipment_type), notice: "Equipment Unit #{resource.name} succesfully saved" }
+      failure.html do
+        flash.now[:alert] = 'Some errors prevented this Equipment Unit from being saved'
+        render view_for_failure
+      end
+    end
+
+    def create
+      super { |success, failure| after_save success, failure, :new }
+    end
+  end
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
